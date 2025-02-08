3404a90019bdd9bb3331d8c1f1292dc147652c0f
eduardacalderan <eduardanicolodicalderan@gmail.com>
2025-02-07 23:08:16 -0300

    Add WebDietAutomation class and integrate Selenium for web tasks
    - Introduced WebDietAutomation class for handling web interactions.
    - Set up Selenium WebDriver and implemented login functionality.
    - Updated logging configuration to manage third-party library log levels.
    - Added dotenv support for environment variable management.

--- src/main.py
+++ src/main.py
@@ -2,7 +2,13 @@
 from configs.logging_config import setup_logging
 import logging 
 
+from rpa.web_diet_integration import WebDietAutomation
+
 if __name__ == "__main__":
   setup_logging()
   logger = logging.getLogger('main:')
   logger.debug('This is a test debug message')
+  
+  web_diet_automation = WebDietAutomation()
+  web_diet_automation.perform_tasks()
+
